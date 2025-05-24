64ae58097735b8d8ccf7f927791121dbabc9bc3a
SauloOliveira358 <saulooliveiram358@gmail.com>
2025-05-23 21:24:50 -0300

    Mechi no manifest e arrumei o login pra entrar so quando digitar algo

--- app/src/main/java/com/example/showdomilhao/TelaNome.java
+++ app/src/main/java/com/example/showdomilhao/TelaNome.java
@@ -1,13 +1,35 @@
 package com.example.showdomilhao;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class TelaNome extends AppCompatActivity {
-
+private Button continuar;
+private EditText nome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_nome);
+
+        continuar = findViewById(R.id.idBtnContinuar);
+        nome = findViewById(R.id.idEditNomeUsuario);
+
+
+            continuar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(!nome.getText().toString().trim().isEmpty()) {
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                    }
+                }
+            });
+
     }
+
 }
