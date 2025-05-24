2552af7e175e16fe1a5aaabb3de1c3f071426953
thiag <thiago.edu.alves502@gmail.com>
2025-05-24 18:00:36 -0300

    So fiz q o main seja a principal

--- app/src/main/java/com/example/showdomilhao/MainActivity.java
+++ app/src/main/java/com/example/showdomilhao/MainActivity.java
@@ -1,17 +1,18 @@
 package com.example.showdomilhao;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+
 public class MainActivity extends AppCompatActivity {
 
-    private Button iniciar,placar,comoJogar,sair;
+    private Button iniciar, placar, comoJogar, sair;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,35 +21,40 @@ public class MainActivity extends AppCompatActivity {
 
         iniciar = findViewById(R.id.idBtnIniciar);
         placar = findViewById(R.id.idBtnPlacar);
-        placar = findViewById(R.id.idBtnComoJogar);
+        comoJogar = findViewById(R.id.idBtnComoJogar);
         sair = findViewById(R.id.idBtnSair);
 
-        iniciar.setOnClickListener(v ->{
-            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        iniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
 
-            LayoutInflater inflater =  getLayoutInflater();
-            View dialogView = inflater.inflate(R.layout.activity_tela_nome, null);
-            builder.setView(dialogView);
+                LayoutInflater inflater = getLayoutInflater();
+                View dialogView = inflater.inflate(R.layout.activity_tela_nome, null);
+                builder.setView(dialogView);
 
-            AlertDialog dialog = builder.create();
-            dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+                AlertDialog dialog = builder.create();
+                dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+                dialog.show();
 
-            EditText nome = dialogView.findViewById(R.id.idEditNomeUsuario);
-            Button continuar = dialogView.findViewById(R.id.idBtnContinuar);
+                EditText nome = dialogView.findViewById(R.id.idEditNomeUsuario);
+                Button continuar = dialogView.findViewById(R.id.idBtnContinuar);
 
-
-            continuar.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(getApplicationContext(),Quiz.class);
-                    startActivity(intent);
-                }
-            });
+                continuar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(getApplicationContext(), Quiz.class);
+                        startActivity(intent);
+                        dialog.dismiss();
+                    }
                 });
-
+            }
+        });
+        sair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              finish();  ;
+            }
+        });
     }
-
-
-
-
 }
